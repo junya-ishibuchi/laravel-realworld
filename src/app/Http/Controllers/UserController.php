5d597bdca1e3cf8973dc8b8129b26ca6092289e5
Junya <junya.ishibuchi@gmail.com>
2024-01-17 09:16:48 -0800

    feat: implement register users

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function create(Request $request): JsonResponse
+    {
+        // TODO: move to Request class
+        $request->validate([
+            'user.username' => 'required|string|max:255',
+            'user.email' => 'required|string|lowercase|email|max:255|unique:users,email',
+            'user.password' => ['required'],
+        ]);
+
+        // TODO: create service and repository layer
+        $user = User::create([
+            'name' => $request->input('user.username'),
+            'email' => $request->input('user.email'),
+            'password' => Hash::make($request->input('user.password')),
+        ]);
+
+        $token = auth()->attempt(['email' => $request->input('user.email'), 'password' => $request->input('user.password')]);
+
+        return response()->json([
+            "user" => [
+                "email" => $user->email,
+                "token" => $token,
+                "username" => $user->name,
+                "bio" => $user->bio,
+                "image" => $user->image
+            ]
+        ]);
+    }
+}
